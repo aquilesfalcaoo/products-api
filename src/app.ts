d799a23444130382cb54542b5e98d1e4f34fb101
Aquiles <aquilesfalcaoo@gmail.com>
2024-07-15 21:06:12 -0300

    feat: adicionado console.log pra debugar cors origin

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,8 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+console.log("corsOptions:", corsOptions);
+
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
